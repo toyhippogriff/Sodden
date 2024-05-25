7ebfac27c4156d384c579675104b6ef105043dba
toyhippogriff <toyhippogriff@gmail.com>
2024-05-25 14:53:35 -0500

    Add config screen to Forge version

--- forge/src/main/java/toyhippogriff/sodden/forge/SoddenForge.java
+++ forge/src/main/java/toyhippogriff/sodden/forge/SoddenForge.java
@@ -1,9 +1,13 @@
 package toyhippogriff.sodden.forge;
 
 import dev.architectury.platform.forge.EventBuses;
+import net.minecraftforge.client.ConfigGuiHandler;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import toyhippogriff.sodden.Sodden;
+import toyhippogriff.sodden.config.SoddenConfig;
 
 @Mod(Sodden.MOD_ID)
 public class SoddenForge
@@ -13,5 +17,13 @@ public class SoddenForge
         EventBuses.registerModEventBus(Sodden.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
 
         Sodden.init();
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+    }
+
+    public void clientSetup(FMLClientSetupEvent event)
+    {
+        ModLoadingContext.get().registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () ->
+            new ConfigGuiHandler.ConfigGuiFactory((client, screen) -> SoddenConfig.getConfigScreen(screen)));
     }
 }
